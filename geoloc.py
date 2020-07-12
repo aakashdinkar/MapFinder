dc5a7c6c854673b54068b6ef940416c75cdf5b39
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-12 12:28:55 +0530

    added new functionality

--- geoloc.py
+++ geoloc.py
@@ -1,23 +1,24 @@
 import requests
 import json
 import folium
+from collections import namedtuple
+import numpy as np
+import math
+from functions import getarrow, distance
 
 
-api_key = False
-if api_key is False:
-	api_key = 42
-	baseurl = "http://py4e-data.dr-chuck.net/json"
-else:
-	baseurl = "https://maps.googleapis.com/maps/api/geocode/json"
+baseurl = "http://py4e-data.dr-chuck.net/json"
 
-while True:
+i = 0
+loc, place = [], []
+while i != 2:
+	i += 1
 	address = input("Enter address: ")
 	if len(address) < 1: break
 
 	params = dict()
 	params['address'] = address
-	if api_key is not False:
-		params['key'] = api_key
+	params['key'] = 42
 	req = requests.get(baseurl, params = params)
 	
 	data = req.text
@@ -34,13 +35,22 @@ while True:
 	lng = js['results'][0]['geometry']['location']['lng']
 	location = js['results'][0]['formatted_address']
 	print("Location: ",location)
+	loc.append([lat,lng])
+	place.append(location)
 	print("lat {}, lng {}".format(lat,lng))
-	m = folium.Map(
-		location=[lat, lng],
-		zoom_start = 12
-	)
-	tooltip = 'Click me!'
 
-	folium.Marker([lat, lng], popup='<i>Mt. Hood Meadows</i>', tooltip=tooltip).add_to(m)
-	m.save("index.html")
-	break
+
+dist_map = folium.Map(location=loc[0], zoom_start=12)
+folium.Marker(location=loc[0], icon=folium.Icon(color='green') , popup = place[0]).add_to(dist_map)
+folium.Marker(location=loc[1], icon=folium.Icon(color='blue'), popup = place[1]).add_to(dist_map)
+
+folium.PolyLine(locations=[loc[0], loc[1]], color='red').add_to(dist_map)
+arrows = getarrow.getArrows(locations=[loc[0], loc[1]], n_arrows=5)
+
+for arrow in arrows:
+    arrow.add_to(dist_map)
+
+dist_map.save('index.html')
+
+dis = distance.find_distance((loc[0][0], loc[0][1]), (loc[1][0], loc[1][1]))
+print("Aerial Distance between %s and %s is %d Km"%(place[0], place[1] ,dis))
